ba6c8c34dad135c8edfbc7fd349867b34fb0f9b7
MaxSaunders <56704552+MaxSaunders@users.noreply.github.com>
2019-11-28 21:35:05 -0500

    Update smorseCode.java

--- smorseCode.java
+++ smorseCode.java
@@ -1,10 +1,6 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
+/*
+Java program to convert given words to their morse code equivalent
+*/
 
 public class Main
 {
